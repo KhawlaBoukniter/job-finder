411e35c7da0c3f30b0694df23e5e390f3a146b9c
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-02-10 21:42:39 +0100

    feat: configure app routing with lazy loaded features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { JobsComponent } from './features/jobs/jobs/jobs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'jobs', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'jobs', component: JobsComponent },
+  { 
+    path: 'favorites', 
+    loadComponent: () => import('./features/favorites/favorites/favorites.component')
+      .then(m => m.FavoritesComponent) 
+  },
+  { 
+    path: 'applications', 
+    loadComponent: () => import('./features/applications/applications/applications.component')
+      .then(m => m.ApplicationsComponent) 
+  }
+];
