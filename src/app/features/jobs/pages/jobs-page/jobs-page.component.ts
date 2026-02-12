a0862152a8f28c05f08506adfcc1adabef81e5ad
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-02-12 20:49:31 +0100

    feat(jobs): add simple search form with keyword and location validation

--- src/app/features/jobs/pages/jobs-page/jobs-page.component.ts
+++ src/app/features/jobs/pages/jobs-page/jobs-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-jobs-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './jobs-page.component.html',
   styleUrl: './jobs-page.component.css'
 })
 export class JobsPageComponent {
+  private fb = inject(FormBuilder);
 
+  searchForm = this.fb.group({
+    keyword: ['', Validators.required],
+    location: ['', Validators.required]
+  });
+
+  onSearch(): void {
+    if (this.searchForm.invalid) return;
+
+    // Placeholder for search logic
+    console.log('Search values:', this.searchForm.value);
+  }
 }
