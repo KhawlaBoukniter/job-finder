7eca4ca779110388c2c3de361c3d705c8d0d2bf4
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-02-12 21:32:56 +0100

    feat(favorites): create favorites feature store structure

---
+++ src/app/features/favorites/store/favorites.state.ts
@@ -0,0 +1,17 @@
+import { Job } from '../../jobs/models/job.model';
+
+export interface Favorite extends Job {
+    userId: string;
+}
+
+export interface FavoritesState {
+    favorites: Favorite[];
+    isLoading: boolean;
+    error: string | null;
+}
+
+export const initialFavoritesState: FavoritesState = {
+    favorites: [],
+    isLoading: false,
+    error: null
+};
