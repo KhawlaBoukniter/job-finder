b1d8d86113ce9626a72b9a12afc3dcb4b2d3ea7f
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-02-13 13:50:44 +0100

    style(ui): update color theme to purple and zinc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
+import { RegisterPageComponent } from './features/auth/pages/register-page/register-page.component';
 import { JobsPageComponent } from './features/jobs/pages/jobs-page/jobs-page.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'jobs', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterPageComponent },
   { path: 'jobs', component: JobsPageComponent },
   {
     path: 'favorites',
